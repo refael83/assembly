df1fc0f48e04c45e0c052ea68b01226c8567c13e
refael83 <126816794+refael83@users.noreply.github.com>
2024-01-15 00:02:58 +0200

    Add files via upload

--- mmn11_4.asm
+++ mmn11_4.asm
@@ -10,7 +10,7 @@ msg2: .asciiz "Error: Invalid input. Please enter a valid number.\n"
 p: .asciiz  "p "
 b: .asciiz  "b "
 bool: .word 0,0,0
-guess:  .word 0,0,0
+guess:  .space 32
 input_guess: .space 32 
 ################# Code segment #####################
 .text
@@ -34,39 +34,39 @@ input:    li $v0, 12       # System call for read_int
           addi $t0,$t0,1
           blt  $t0,3,input
  
- get_guess:la $a2,guess
+ get_guess:
+ 	   la $a0,guess
            add $a1,$0,23
            sub $t0,$t0,$t0
            li $v0, 8 #stored in address of a0
-           la $a0,input_guess
 	   syscall
 	   
 insert_to_array:
 	
-       lb $t2,input_guess($t0)
-       blt $t2, '0', invalid_guess_input
-       bgt $t2, '9', invalid_guess_input
-       sb  $t2,0($a2)
-       addi $a2,$a2,1 
-       addi $t0,$t0,1
-       blt  $t0,3,insert_to_array
        
-       	la $a0,guess #address of guess array
+       #sb  $t2,0($a2)
+       #addi $a2,$a2,1 
+       #addi $t0,$t0,1
+       #blt  $t0,3,insert_to_array
+       
+       	la $a2,guess #address of guess array
 	la $a1,bool  #address of bool array   
 loop:
-	lw $t0,0($a0) #t0 = guess[t3]
+	lb $t2,guess($t0) #t0 = guess[t3]
+	blt $t2, '0', invalid_guess_input
+        bgt $t2, '9', invalid_guess_input
 loop2:
 	lw $t1,0($a1) #t1 = bool[t4]
-	sub $t2,$t1,$t0
+	sub $t6,$t1,$t2
 	sub $t5,$t4,$t3
-	beqz $t2,equal
+	beqz $t6,equal
 continu:
 	addi  $t4,$t4,1
 	addi  $a1,$a1,4
 	blt  $t4,2,loop2
 	
 	addi $t3,$t3,1
-	addi $a0,$a0,4
+	addi $t0,$t0,1
 	blt $t3,2,loop	
 	
 	  # Exit the program
@@ -89,12 +89,12 @@ invalid_guess_input:
         j get_guess
 
 equal:
-	beqz $t5,boo
+	beqz $t5,print_b
 	la $a0, p
     	li $v0,4
     	syscall
     	j continu
-boo:
+print_b:
 	la $a0, b
     	li $v0,4
     	syscall
